6753649baedff5422b438280731a42776f4023a5
Rully (eirworks) <hvsciverius@gmail.com>
2024-09-26 14:13:19 +0700

    add gallery

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <div v-if="props.images.length === 0" class="p-10 text-center text-gray-500">
+            Tidak ada gambar
+        </div>
+        <template v-else>
+            <div class="flex justify-center mb-5 items-center space-x-3">
+                <div>
+                    <button type="button" class="rounded border border-black hover:bg-gray-200 p-3" @click="previousImage()">&larr;</button>
+                </div>
+                <div class="w-1/2 h-full bg-gray-500">
+                    <img :src="currentImage" alt="Product Image">
+                </div>
+                <div><button type="button" class="rounded border border-black hover:bg-gray-200 p-3" @click="nextImage()">&rarr;</button></div>
+            </div>
+            <div class="my-5 flex justify-center space-x-3 flex-wrap">
+                <div v-for="img, idx in props.images">
+                    <div class="bg-gray-500 cursor-pointer w-[50px] h-[50px]" :class="{'border-2 border-black shadow': currentImage == img}">
+                        <img :src="img" :alt="'Image '+(idx+1)" v-on:click="setImageByIndex(idx)">
+                    </div>
+                </div>
+            </div>
+        </template>
+        
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+    'images': {
+        type: Array,
+        default: []
+    }
+})
+
+const currentImage = ref()
+const currentIndex = ref(0)
+
+if (props.images.length > 0) {
+    currentImage.value = props.images[0]
+}
+
+function previousImage() {
+    if (currentIndex.value === 0) {
+        currentIndex.value = props.images.length - 1
+    }
+    else {
+        currentIndex.value--
+    }
+
+    setImageByIndex(currentIndex.value)
+}
+function nextImage() {
+    if (currentIndex.value === props.images.length - 1) {
+        currentIndex.value = 0
+    }
+    else {
+        currentIndex.value++
+    }
+
+    setImageByIndex(currentIndex.value)
+}
+
+function setImageByIndex(idx: number) {
+    currentImage.value = props.images[idx]
+    currentIndex.value = idx
+}
+</script>
--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -7,62 +7,51 @@
         </div>
         <div v-if="store.product !== null">
             <div class="font-bold text-2xl mb-5">{{ store.product.title }}</div>
-        <div class="flex justify-center mb-5 items-center space-x-3">
-            <div>
-                <button type="button" class="rounded border border-black hover:bg-gray-200 p-3">&larr;</button>
-            </div>
-            <div class="w-1/2 h-[400px] bg-gray-500"></div>
-            <div><button type="button" class="rounded border border-black hover:bg-gray-200 p-3">&rarr;</button></div>
-        </div>
-        <div class="my-5 flex justify-center space-x-3 flex-wrap">
-            <div v-for="n in 5">
-                <div class="bg-gray-500 cursor-pointer w-[50px] h-[50px]" :class="{'border border-black shadow': n == 1}"></div>
-            </div>
-        </div>
-        <div class="my-5">
-            <div class="grid md:grid-cols-3 gap-5">
-                <div class="col-span-2">
-                    <div class="mb-3 text-sm text-gray-500">Tentang produk ini</div>
+            <ImageGallery :images="store.product.image_urls" />
+            <div class="my-5">
+                <div class="grid md:grid-cols-3 gap-5">
+                    <div class="col-span-2">
+                        <div class="mb-3 text-sm text-gray-500">Tentang produk ini</div>
+                        <div>
+                            {{ store.product.description }}
+                        </div>
+                    </div>
                     <div>
-                        {{ store.product.description }}
+                        <DataItem name="Ukuran Tersedia:">
+                            <div class="flex space-x-3">
+                                <div v-for="s in store.product.sizes" 
+                                    class="uppercase" 
+                                    :key="s">{{ s }}</div>
+                            </div>
+                        </DataItem>
+                        <DataItem name="Warna">
+                            <div class="flex space-x-3">
+                                <div v-for="s in store.product.colors" 
+                                    class="capitalize" 
+                                    :key="s">{{ s }}</div>
+                            </div>
+                        </DataItem>
+                        <DataItem name="Material">
+                            {{ store.product.material }}
+                        </DataItem>
+                        
                     </div>
                 </div>
-                <div>
-                    <DataItem name="Ukuran Tersedia:">
-                        <div class="flex space-x-3">
-                            <div v-for="s in store.product.sizes" 
-                                class="uppercase" 
-                                :key="s">{{ s }}</div>
-                        </div>
-                    </DataItem>
-                    <DataItem name="Warna">
-                        <div class="flex space-x-3">
-                            <div v-for="s in store.product.colors" 
-                                class="capitalize" 
-                                :key="s">{{ s }}</div>
-                        </div>
-                    </DataItem>
-                    <DataItem name="Material">
-                        {{ store.product.material }}
-                    </DataItem>
-                    
-                </div>
+                
             </div>
-            
-        </div>
-        <ShopList center get-it-on />
-        <Contacts />
+            <ShopList center get-it-on />
+            <Contacts />
         </div>
     </div>
 </template>
 
 <script setup>
 import DataItem from '../components/DataItem.vue'
-import { shops, contacts } from '@/site';
 import ShopList from '@/components/ShopList.vue';
 import Contacts from '@/components/Contacts.vue';
 import { useViewProductStore } from '@/store/products';
 import { useRoute } from 'vue-router';
+import ImageGallery from '@/components/ImageGallery.vue';
 
 const route = useRoute()
 const store = useViewProductStore()
