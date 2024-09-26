f55f1a6e7fa9dead7a36debb2fe2888598261a87
Rully (eirworks) <hvsciverius@gmail.com>
2024-09-26 12:47:19 +0700

    add image carousel

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="md:w-2/3 container mx-auto">
         <div class="font-bold text-2xl mb-5 text-center">Product Name</div>
-        <div class="flex justify-center mb-5">
-            <div class="w-[300px] h-[300px] bg-gray-500"></div>
+        <div class="flex justify-center mb-5 items-center space-x-3">
+            <div>
+                <button type="button" class="rounded border border-black hover:bg-gray-200 p-3">&larr;</button>
+            </div>
+            <div class="w-1/2 h-[400px] bg-gray-500"></div>
+            <div><button type="button" class="rounded border border-black hover:bg-gray-200 p-3">&rarr;</button></div>
+        </div>
+        <div class="my-5 flex justify-center space-x-3 flex-wrap">
+            <div v-for="n in 5">
+                <div class="bg-gray-500 cursor-pointer w-[50px] h-[50px]" :class="{'border border-black shadow': n == 1}"></div>
+            </div>
         </div>
         <div class="my-5">
             <div class="grid md:grid-cols-2 gap-3">
